b43eaa1682a0bb2115d5ad6a4d36beb2995d06f3
dongxiaoman <dongxiaoman@gmail.com>
2021-10-26 23:30:09 -0700

    simple refactor

--- hangman_game/Hangman.py
+++ hangman_game/Hangman.py
@@ -1,32 +1,23 @@
 import time
 import random
 
-print('Welcome to Hangman game')
-print('\n')
-name = input('Enter your name:')
-time.sleep(1)
-print('Hello, %s! Best of Luck!' % name)
-time.sleep(2)
-print('     Let\'s play Hangman!')
 
-
-class Hangman():
+class Hangman:
 
     def __init__(self):
-        self.words_list = ["january", "border", "image", "film", "promise", "kids", "lungs", "doll", "rhyme", "damage", "plants"]
+        self.words_list = ["january", "border", "image", "film", "promise", "kids", "lungs", "doll", "rhyme", "damage",
+                           "plants"]
 
-
-    def playloop(self):
+    def play_another_game(self):
         print('Do you want to play again? y = yes, n = no')
         ans = input('')
         if ans == 'y':
-            self.hangman()
-        elif ans=='n':
+            self.play_one_game()
+        elif ans == 'n':
             print("Thanks For Playing! We expect you back again!")
             exit()
 
-
-    def hangman(self):
+    def play_one_game(self):
         count = 0
         already_guessed = ''
         self.word_to_be_guessed = random.choice(self.words_list)
@@ -41,33 +32,33 @@ class Hangman():
             if not char_guessed.isalpha():
                 print('Invalid Input, Try a letter')
             else:
-                if char_guessed  in self.word_to_be_guessed:
-                    new_guessed=''
+                if char_guessed in self.word_to_be_guessed:
+                    new_guessed = ''
                     for char_to_be_guessed, char_in_alreay_guessed in zip(self.word_to_be_guessed, already_guessed):
                         if char_to_be_guessed == char_guessed:
                             new_guessed += char_guessed
                         else:
                             new_guessed += char_in_alreay_guessed
                     already_guessed = new_guessed
-                    if already_guessed ==self.word_to_be_guessed:
+                    if already_guessed == self.word_to_be_guessed:
                         print(already_guessed)
                         print('\n')
                         print('Congrats! YOu have guessed the word correctly!')
                         break
 
                 else:
-                    count +=1
+                    count += 1
                     if count == 1:
                         time.sleep(1)
-                        print ('  _____\n'
-                               '  |    \n'
-                               '  |    \n'
-                               '  |    \n'
-                               '  |    \n'
-                               '  |    \n'
-                               '  |    \n'
-                               '  |    \n'
-                               '__|__  \n')
+                        print('  _____\n'
+                              '  |    \n'
+                              '  |    \n'
+                              '  |    \n'
+                              '  |    \n'
+                              '  |    \n'
+                              '  |    \n'
+                              '  |    \n'
+                              '__|__  \n')
 
                         print('Wrong guess. 4 guesses remaining')
 
@@ -129,10 +120,4 @@ class Hangman():
                         print('The word was:', self.word_to_be_guessed)
                         break
 
-        self.playloop()
-
-
-
-
-
-Hangman().hangman()
+        self.play_another_game()
